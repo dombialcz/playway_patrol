9b671d29983a8e6e2d79b5a19f29f1be251f6bf0
Dominik  Bialczyk <Dominik.Bialczyk@netent.com>
2020-12-01 21:01:27 +0100

    init patrol

---
+++ Assets/Patrol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+  
+public class Patrol : MonoBehaviour
+{
+
+ public float moveSpeed = 2.0f;  // Units per second
+ public List<GameObject> patrolList = new List<GameObject>();
+ private GameObject nextTarget = null;
+ private int current = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+		SetNextPoint() ;
+	}
+  
+	 void Update () {
+		 move();
+	 }
+
+	void move () {
+		if (nextTarget == null)
+			return;
+        transform.position = Vector3.MoveTowards (transform.position, nextTarget.transform.position, 3.0f * Time.deltaTime);
+		if (Vector3.Distance (transform.position, nextTarget.transform.position) <= 0.1f)
+			SetNextPoint();
+	}
+
+	void SetNextPoint() {
+		if (current >= patrolList.Count())
+		current = 0;
+
+		nextTarget = patrolList[current++];
+	}
+
+	// test only
+	void moveToCursor() {
+	     if (Input.GetMouseButton(0)) {
+	         Vector3 pos = Input.mousePosition;
+	         pos.z = transform.position.z - Camera.main.transform.position.z;
+	         pos = Camera.main.ScreenToWorldPoint(pos);
+	         transform.position = Vector3.MoveTowards(transform.position, pos, moveSpeed * Time.deltaTime);
+	     }
+	}
+
+	 public void updatePatrolList(List<GameObject> patrolList){
+		 this.patrolList = patrolList;
+		SetNextPoint();
+	 }
+}
---
+++ Assets/tester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tester : MonoBehaviour
+{
+
+	public GameObject kulka;
+    private Patrol patrol;
+    public bool isPatrol = false;
+    public GameObject[] patrolPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            togglePatrol();
+            UpdatePatrolList();
+        }
+    }
+
+    void UpdatePatrolList () {
+        kulka.GetComponent<Patrol>().updatePatrolList(new List<GameObject>(patrolPoints));
+    }
+
+    void togglePatrol(){
+        kulka.GetComponent<Patrol>().enabled = !kulka.GetComponent<Patrol>().enabled;
+    }
+}
